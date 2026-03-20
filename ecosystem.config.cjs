57dd52021548c4248b6fdac72e2a670e3ffadb8d
chenjy1119 <chenjy1119@thundersoft.com>
2026-03-20 20:12:55 +0800

    Fix EC2 Nitro binding to allow external connections

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,7 +7,9 @@ module.exports = {
       instances: 1, // Using 1 instance because SQLite does not natively support multi-process concurrent writes well
       script: './.output/server/index.mjs',
       env: {
-        NODE_ENV: 'production'
+        NODE_ENV: 'production',
+        NITRO_PORT: 3000,
+        NITRO_HOST: '0.0.0.0'
       }
     }
   ]
